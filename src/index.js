67d22bce63d43a7d12daa8932f58abd9862cdad9
Rodney Bejabeng <Rodneyb2@illinois.edu>
2022-07-28 14:44:45 -0500

    Change Square class to be a function component

--- src/index.js
+++ src/index.js
@@ -2,24 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: null,};
-    }
-    render() {
-      return (
-        <button className='square' onClick={() => {console.log('click'); }}>
-          {this.props.value}
-        </button>
-      );
-    }
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
   
   class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={i} />;
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
     }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+  
+    renderSquare(i) {
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
+    }
+  
   
     render() {
       const status = 'Next player: X';
